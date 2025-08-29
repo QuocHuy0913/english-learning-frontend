4cf6129df47902eea59cbf5d66bb15cd70f3e0a8
QuocHuy0913 <www.shirokuma@gmail.com>
2025-08-29 11:10:35 +0700

    update quesion report

--- src/pages/Client/QuestionDetail.vue
+++ src/pages/Client/QuestionDetail.vue
@@ -136,7 +136,7 @@ onMounted(loadData);
 </script>
 <template>
   <div class="row">
-    <div class="container mt-4"
+    <div class="container mt-4 px-3"
          style="max-width: 720px;">
       <button class="btn btn-outline-success mb-3 fw-semibold"
               @click="router.back()">
